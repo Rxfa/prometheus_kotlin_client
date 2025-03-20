c742a10b6c63c12848af857da5e4399cecde7adb
rxfa <rafa3rnrn@gmail.com>
2025-03-20 16:03:36 +0000

    refactor: make it so there is only a common implementation of CollectorRegistry

--- library/src/commonMain/kotlin/CollectorRegistry.kt
+++ library/src/commonMain/kotlin/CollectorRegistry.kt
@@ -1,18 +1,51 @@
 package io.github.kotlin.fibonacci
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 
-expect class CollectorRegistry {
-    suspend fun register(collector: Collector)
 
-    suspend fun unregister(collector: Collector)
+class CollectorRegistry {
+    private val mutex = Mutex()
+    private val collectorNames = mutableSetOf<String>()
+    private var collectors = listOf<Collector>()
 
-    suspend fun clear()
+    suspend fun register(collector: Collector) {
+        val collectorName = collector.getClassName()
+        val addedToRegistry = mutex.withLock { collectorNames.add(collectorName) }
+        if (!addedToRegistry) {
+            throw IllegalStateException("Collector is already registered: $collectorName")
+        }
+        mutex.withLock { this.collectors += collector }
+    }
 
-    suspend fun collect(): List<Collector>
+    suspend fun unregister(collector: Collector) {
+        val collectorName = collector.getClassName()
+        mutex.withLock {
+            if(this.collectorNames.contains(collectorName)) {
+                this.collectors -= collector
+            }
+            this.collectorNames.remove(collectorName)
+        }
+    }
 
-    suspend fun getCollectors(): List<Collector>
+    suspend fun clear() {
+        mutex.withLock {
+            this.collectors = listOf()
+            this.collectorNames.clear()
+        }
+    }
+
+    suspend fun collect(): List<Collector> {
+        return withContext(Dispatchers.Default) { getCollectors() }
+    }
 
     companion object {
-        val defaultRegistry: CollectorRegistry
+        val defaultRegistry = CollectorRegistry()
+    }
+
+    suspend fun getCollectors(): List<Collector> {
+        return mutex.withLock { collectors.toList() }
     }
 }
