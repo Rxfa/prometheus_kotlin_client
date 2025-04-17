7181adbe4cf088c9771bb32321af8a9461e0606d
rxfa <rafa3rnrn@gmail.com>
2025-04-17 04:46:16 +0100

    refactor: Counter now has common implementation

--- library/src/commonMain/kotlin/Counter.kt
+++ library/src/commonMain/kotlin/Counter.kt
@@ -1,28 +1,59 @@
 package io.github.kotlin.fibonacci
 
-expect class Counter(
+class Counter(
     fullName: String,
     help: String,
-    labelNames: List<String> = emptyList(),
-    unit: String = "",
-    includeCreatedSeries: Boolean = false,
-): SimpleCollector<Counter.Child> {
-    override fun newChild(): Child
+    labelNames: List<String>,
+    unit: String,
+    val includeCreatedSeries: Boolean,
+) : SimpleCollector<Counter.Child>(fullName, help, labelNames, unit) {
+    override val suffixes: Set<String> = setOf("_total")
 
-    inner class Child {
-        fun inc(amount: Double)
+    override val name: String = if(suffixes.any{ fullName.endsWith(it) }) fullName else fullName + "_total"
 
-        fun inc()
+    override val type: Type = Type.COUNTER
 
-        fun get(): Double
+    init {
+        initializeNoLabelsChild()
     }
 
+    override fun newChild(): Child {
+        return Child()
+    }
 
-    fun inc(amount: Double): Unit?
+    inner class Child {
+        private var value = 0.0
 
-    fun inc(): Unit?
+        fun inc(amount: Double) {
+            require(amount >= 0) { "Value must be positive" }
+            value += amount
+        }
 
-    fun get(): Double
+        fun inc(){
+            value += 1.0
+        }
 
-    override fun collect(): MetricFamilySamples
+        fun get(): Double = value
+    }
+
+    fun inc(amount: Double): Unit? {
+        require(amount >= 0) { "Amount must be positive" }
+        return noLabelsChild?.inc(amount)
+    }
+
+    fun inc(): Unit? = noLabelsChild?.inc()
+
+    fun get(): Double = noLabelsChild?.get() ?: 0.0
+
+    override fun collect(): MetricFamilySamples {
+        val samples = mutableListOf<Sample>()
+        for ((labels, child) in childMetrics){
+            samples += Sample(name = name, labelNames = labelNames, labelValues = labels, value = child.get())
+            if(includeCreatedSeries){
+                val createdSeriesName = name.removeSuffix("_total") + "_created"
+                samples += Sample(name = createdSeriesName, labelNames = labelNames, labelValues = labels, value = child.get())
+            }
+        }
+        return familySamplesList(samples)
+    }
 }
--- library/src/nativeMain/kotlin/Collector.native.kt
+++
@@ -1,5 +0,0 @@
-package io.github.kotlin.fibonacci
-
-actual fun Collector.getClassName(): String {
-    TODO("Not implemented yet.")
-}
