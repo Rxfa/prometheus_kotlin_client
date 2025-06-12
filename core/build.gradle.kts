5d34a982022a7b0e6451ced5a515e54ecd1c50ea
Rafa <65974055+Rxfa@users.noreply.github.com>
2025-06-12 20:41:29 +0100

    Beta delivery (#10)
    * docs: add report for beta delivery
    
    * docs(report): add abstract
    
    * docs: write chapter 1 (introduction)
    
    * docs: write chapter 2 (Background)
    
    * docs: add prometheus arch figure
    
    * added architecture conclusion evaluation
    
    ---------
    
    Co-authored-by: Mário <A50561@alunos.isel.pt>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -60,10 +60,10 @@ mavenPublishing {
     coordinates(group.toString(), "core", version.toString())
 
     pom {
-        name = "My library"
-        description = "A library."
-        inceptionYear = "2024"
-        url = "https://github.com/kotlin/multiplatform-library-template/"
+        name = "Prometheus Kotlin client"
+        description = "Prometheus client library written in Kotlin"
+        inceptionYear = "2025"
+        url = "https://github.com/Rxfa/prometheus_kotlin_client/"
         licenses {
             license {
                 name = "XXX"
